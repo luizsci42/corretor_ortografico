d4323acd52fac4984038076fae62d51676a62b8f
luizsci42 <luizsci42@gmail.com>
2021-02-09 19:28:49 -0300

    Versão final

--- funcoes.hs
+++ funcoes.hs
@@ -1,27 +1,17 @@
 module Funcoes (
     dicionario,
     buscaDicionario,
-    adicionar
+    adicionar,
+    ignorar,
+    ignorados
 ) where
 
 -- Aqui temos um dicionário
 dicionario :: [String]
 dicionario = ["Panafricanismo", "Diop", "Clarke"]
--- e uma lista de entrada
-demo :: [String]
-demo = ["OI", "Panafricanismo", "Diop", "Malcolm", "Manimba", "Clarke", "Maninba"]
 -- lista de termos ignorados
 ignorados :: [String]
-ignorados = []
-
--- mostra quais palavras não estão presentes no dicionário
-comparar :: [String] -> [String]
-comparar (x:xs)
-    -- caso base. Quando todos os termos da lista estão no dicionário, retorne a própria lista
-    | null xs = [x | x <- x:xs]-- quando o termo existir no dicionário, vá para o próximo termo
-    | buscaDicionario x dicionario = comparar xs
-    | not (buscaDicionario x dicionario) = adicionar x dicionario
-    | otherwise = comparar xs
+ignorados = [""]
 
 -- pega um elemento da lista e procura no dicionário
 -- retorna False quando a palavra não existe no dicionário
@@ -39,14 +29,3 @@ adicionar termo dic = termo:dic
 
 ignorar :: String -> [String]
 ignorar termo = termo:ignorados
-
-mostrarLista :: [String] -> String 
-mostrarLista (x:xs)
-    | null xs = x
-    | otherwise = mostrarLista xs
-
--- vamos comparar as palavras na lista de entrada e
--- consultar o usuário para qual decisão deve ser
--- tomada quando encontrarmos uma palavra desconhecida.
-teste :: [String]
-teste = comparar demo
