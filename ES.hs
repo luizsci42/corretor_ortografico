561e1fdd573263a9bfec1e8913e3612c9363a485
luizsci42 <luizsci42@gmail.com>
2021-02-09 18:36:28 -0300

    Opções de substituir, ignorar e adicionar funcionando

--- ES.hs
+++ ES.hs
@@ -1,8 +1,11 @@
 module ES (es, exec) where
 import Funcoes
 
-es :: [String] -> IO ()
-es dic = do
+listaSaida :: [String]
+listaSaida = []
+
+es :: [String] -> [String] -> [String] -> IO ()
+es dic saida ign = do
     _ <- putStrLn "Digite algumas palavras. Para parar, entre com uma linha em branco"
     linha <- getLine
     -- se a linha não for nula, verifique se a palavra existe no dicionario
@@ -10,6 +13,8 @@ es dic = do
         -- se a palavra existe no dicionário
         if buscaDicionario linha dicionario then do
             print "Existe no dicionario"
+            let saidaAtual = adicionar linha saida
+            es dic saidaAtual ign
         -- se a palavra não existe no dicionário
         else do
             -- pergunte ao usuário se ele quer adicionar ao dicionário, substituir ou ignorar
@@ -17,17 +22,24 @@ es dic = do
             putStrLn "Deseja adicionar ao dicionario (a), substituir (s) ou ignorar (i)?"
             decisao <- getLine
             if decisao == "a" then do
+                -- adicionar ao dicionário e atualizar a lista de saída
                 let dicAtualizado = adicionar linha dic
                 print "Adicionado!"
-                es dicAtualizado
+                let saidaAtual = adicionar linha saida
+                es dicAtualizado saidaAtual ign
             else do
                 return ()
             if decisao == "s" then do
-                print "Substituir"
+                _ <- putStrLn "Digite uma nova palavra"
+                es dic saida ign
             else do
                 return ()
             if decisao == "i" then do
+                -- Atualizar a saida e a lista de saida
                 print "ignorar"
+                let ignAtualizado = ignorar linha
+                let saidaAtual = adicionar linha saida
+                es dic saidaAtual ignAtualizado
             else do
                 return ()
             if decisao /= "a" && decisao /= "s" && decisao /= "i" then do
@@ -36,6 +48,10 @@ es dic = do
                 return ()
     -- se a linha for vazia, mostre a lista de saída
     else do
-       print dic
+        _ <- putStr "Saida: "
+        print saida
+        _ <- putStr "Ignorados: "
+        print ign
 
-exec = es dicionario
+exec :: IO ()
+exec = es dicionario listaSaida ignorados 
